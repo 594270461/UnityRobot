64679a97b6a288db7597e29f2cb9d32daf31481a
Jaehong, Oh <jhoh@idealink.kr>
2015-02-13 15:21:00 +0900

    v1.0.0b7 배포
    NGUI기반에서 UGUI로 변경

---
+++ unity3d/Assets/UnityRobot/PlayMaker/Actions/UnityRobotGetPortNames.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using UnityRobot;
+
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory("UnityRobot")]
+	[Tooltip("Get Port Names")]
+	public class UnityRobotGetPortNames : FsmStateAction
+	{
+		[RequiredField]
+		public UnityRobot.UnityRobot unityRobot;
+		[UIHint(UIHint.Variable)]
+		public FsmString storeValue;
+		
+		public override void OnEnter()
+		{
+			base.OnEnter();
+			
+			if(unityRobot != null)
+			{
+				string portNames = "";
+				for(int i=0; i<unityRobot.portNames.Count; i++)
+				{
+					if(i > 0)
+						portNames += Environment.NewLine;
+					portNames += unityRobot.portNames[i];
+				}
+				storeValue.Value = portNames;
+			}
+			
+			Finish();
+		}
+	}
+}
